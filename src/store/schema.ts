ce08a7e1c401d9eac73ae6429cd1701f4bc4a7d4
batuhan <batuhan@icoz.co>
2023-07-24 20:33:00 +0200

    docs

--- src/store/schema.ts
+++ src/store/schema.ts
@@ -1,69 +1,81 @@
-import { MessageBehavior } from '@textshq/platform-sdk'
 import { sqliteTable, integer, text, blob } from 'drizzle-orm/sqlite-core'
-// import { relations } from 'drizzle-orm'
 import type { IGMessage, IGParticipant, IGThread } from '../types'
-
-export const threads = sqliteTable('threads', {
-  original: blob('_original', { mode: 'json' }).$type<IGThread>(),
-  id: text('id').primaryKey(),
-  folderName: text('folderName'),
-  title: text('title'),
-  isUnread: integer('isUnread', { mode: 'boolean' }),
-  lastReadMessageID: text('lastReadMessageID'),
-  isReadOnly: integer('isReadOnly', { mode: 'boolean' }),
-  isArchived: integer('isArchived', { mode: 'boolean' }),
-  isPinned: integer('isPinned', { mode: 'boolean' }),
-  mutedUntil: integer('mutedUntil', { mode: 'timestamp' }),
-  type: text('type', { enum: ['single', 'group', 'channel', 'broadcast'] }).notNull().default('single'),
-  timestamp: integer('timestamp', { mode: 'timestamp' }),
-  imgURL: text('imgURL'),
-  createdAt: integer('createdAt', { mode: 'timestamp' }),
-  description: text('description'),
-  partialLastMessage: blob('partialLastMessage'),
-  messageExpirySeconds: integer('messageExpirySeconds'),
-})
+import { InferModel } from 'drizzle-orm'
 
 export const messages = sqliteTable('messages', {
-  original: blob('_original', { mode: 'json' }).$type<IGMessage>(),
-  id: text('id').primaryKey(),
-  timestamp: integer('timestamp', { mode: 'timestamp' }).notNull(),
-  editedTimestamp: integer('editedTimestamp', { mode: 'timestamp' }),
-  expiresInSeconds: integer('expiresInSeconds'),
-  forwardedCount: integer('forwardedCount'),
-  forwardedFromText: text('forwardedFromText'),
-  forwardedFromThreadID: integer('forwardedFromThreadID'),
-  forwardedFromUserID: integer('forwardedFromUserID'),
-  senderID: text('senderID').notNull(),
-  threadID: text('threadID').notNull().references(() => threads.id),
+  original: blob('_original', { mode: 'json' }).$type<Record<string, any>>(),
+  threadKey: text('threadKey').notNull(),
+  offlineThreadingId: text('offlineThreadingId'),
+  authorityLevel: integer('authorityLevel'),
+  timestampMs: integer('timestampMs', { mode: 'timestamp' }),
+  messageId: text('messageId').primaryKey(),
+  senderId: text('senderId').notNull(),
+  isAdminMessage: integer('isAdminMessage', { mode: 'boolean' }),
+  sendStatus: text('sendStatus'),
   text: text('text'),
-  textAttributes: blob('textAttributes'),
-  textHeading: text('textHeading'),
-  textFooter: text('textFooter'),
-  iframeURL: text('iframeURL'),
-  seen: integer('seen', { mode: 'timestamp' }),
-  isDelivered: integer('isDelivered', { mode: 'boolean' }),
-  isHidden: integer('isHidden', { mode: 'boolean' }),
-  isSender: integer('isSender', { mode: 'boolean' }),
-  isAction: integer('isAction', { mode: 'boolean' }),
-  isDeleted: integer('isDeleted', { mode: 'boolean' }),
-  isErrored: integer('isErrored', { mode: 'boolean' }),
-  parseTemplate: integer('parseTemplate', { mode: 'boolean' }),
-  linkedMessageThreadID: text('linkedMessageThreadID'),
-  linkedMessageID: text('linkedMessageID'),
-  action: text('action'),
-  cursor: text('cursor'),
-  behavior: text('behavior', { enum: [MessageBehavior.SILENT, MessageBehavior.KEEP_READ, MessageBehavior.DONT_NOTIFY] }).default(null),
-  accountID: text('accountID'),
-  sortKey: text('sortKey'),
+  subscriptErrorMessage: text('subscriptErrorMessage'),
+  stickerId: text('stickerId'),
+  messageRenderingType: text('messageRenderingType'),
+  isUnsent: integer('isUnsent', { mode: 'boolean' }),
+  unsentTimestampMs: integer('unsentTimestampMs', { mode: 'timestamp' }),
+  mentionOffsets: text('mentionOffsets'),
+  mentionLengths: text('mentionLengths'),
+  mentionIds: text('mentionIds'),
+  mentionTypes: text('mentionTypes'),
+  replySourceId: text('replySourceId'),
+  replySourceType: text('replySourceType'),
 })
 
-// export const threadsRelations = relations(threads, ({ many }) => ({
-//   messages: many(messages),
-// }))
+export type Message = InferModel<typeof messages, 'select'>
+export type NewMessage = InferModel<typeof messages, 'insert'>
 
-export const participants = sqliteTable('participants', {
-  original: blob('original', { mode: 'json' }).$type<IGParticipant>(),
-  threadID: text('threadID').notNull().references(() => threads.id),
-  id: text('id').notNull().primaryKey(),
-  name: text('name'),
+export const messageRanges = sqliteTable('message_ranges', {
+  original: blob('_original', { mode: 'json' }).$type<Record<string, any>>(),
+  threadKey: text('threadKey').notNull(),
+  senderId: text('senderId').notNull(),
+  expirationTimestampMs: integer('expirationTimestampMs', { mode: 'timestamp' }),
+})
+
+export const typingIndicators = sqliteTable('typing_indicators', {
+  original: blob('_original', { mode: 'json' }).$type<Record<string, any>>(),
+  threadKey: text('threadKey').notNull(),
+  minTimestampMs: integer('minTimestampMs', { mode: 'timestamp' }),
+  minMessageId: text('minMessageId'),
+  maxTimestampMs: integer('maxTimestampMs', { mode: 'timestamp' }),
+  maxMessageId: text('maxMessageId'),
+  isLoadingBefore: integer('isLoadingBefore', { mode: 'boolean' }),
+  isLoadingAfter: integer('isLoadingAfter', { mode: 'boolean' }),
+  hasMoreBefore: integer('hasMoreBefore', { mode: 'boolean' }),
+  hasMoreAfter: integer('hasMoreAfter', { mode: 'boolean' }),
+})
+
+export const threads9 = sqliteTable('threads', {
+  original: blob('_original', { mode: 'json' }).$type<Record<string, any>>(),
+  lastReadWatermarkTimestampMs: integer('lastReadWatermarkTimestampMs', { mode: 'timestamp' }),
+  folderName: text('folderName'),
+  parentThreadKey: text('parentThreadKey'),
+  lastActivityTimestampMs: integer('lastActivityTimestampMs', { mode: 'timestamp' }),
+  snippet: text('snippet'),
+  threadName: text('threadName'),
+  threadPictureUrl: text('threadPictureUrl'),
+  needsAdminApprovalForNewParticipant: integer('needsAdminApprovalForNewParticipant', { mode: 'boolean' }),
+  threadPictureUrlFallback: text('threadPictureUrlFallback'),
+  removeWatermarkTimestampMs: integer('removeWatermarkTimestampMs', { mode: 'timestamp' }),
+  muteExpireTimeMs: integer('muteExpireTimeMs', { mode: 'timestamp' }),
+  groupNotificationSettings: text('groupNotificationSettings'), // potentially blob type if it's a complex object
+  isAdminSnippet: integer('isAdminSnippet', { mode: 'boolean' }),
+  snippetSenderContactId: text('snippetSenderContactId'),
+  snippetStringHash: text('snippetStringHash'),
+  snippetStringArgument1: text('snippetStringArgument1'),
+  snippetAttribution: text('snippetAttribution'),
+  snippetAttributionStringHash: text('snippetAttributionStringHash'),
+  disappearingSettingTtl: integer('disappearingSettingTtl'),
+  disappearingSettingUpdatedTs: integer('disappearingSettingUpdatedTs', { mode: 'timestamp' }),
+  disappearingSettingUpdatedBy: text('disappearingSettingUpdatedBy'),
+  cannotReplyReason: text('cannotReplyReason'),
+  customEmoji: text('customEmoji'),
+  customEmojiImageUrl: text('customEmojiImageUrl'),
+  outgoingBubbleColor: text('outgoingBubbleColor'),
+  themeFbid: text('themeFbid'),
+  nullstateDescriptionText1: text('nullstateDescriptionText1'),
 })
