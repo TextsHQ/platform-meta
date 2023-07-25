23b716e1094f11fd35608119ed60ace9a0205596
batuhan <batuhan@icoz.co>
2023-07-25 22:40:12 +0200

    add attachment table, reset migrations

---
+++ src/ig-mappers.ts
@@ -0,0 +1,10 @@
+import { type Message, type Thread } from '@textshq/platform-sdk'
+import type { IGThread } from './ig-types'
+
+export function mapThread(thread: IGThread): Thread {
+  return null
+}
+
+export function mapMessage(currentUserId: string, message: any): Message {
+  return null
+}
--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,5 @@
-import type { Message, Participant, PlatformAPI, Thread } from '@textshq/platform-sdk'
-
-export interface ChatStoreInterface {
-  upsertParticipant(participant: Participant): Promise<void>
-  getParticipant(id: Participant['id']): Promise<Participant | undefined>
-  upsertThread(thread: Thread): Promise<void>
-  addThreads(threads?: Thread[]): Promise<void>
-  getThreads(): Promise<Thread[]>
-  getThread(threadID: string): Promise<Thread | undefined>
-  addMessage(message: Message): Promise<void>
-  addMessages(messages?: Message[]): Promise<void>
-  getMessages(threadID: string): Promise<Message[]>
-  getAllMessages(): Promise<Message[]>
-}
+import type { PlatformAPI } from '@textshq/platform-sdk'
 
 export type MethodReturnType<T, K extends keyof T> = T[K] extends (...args: any[]) => infer R ? R : never
 
 export type PAPIReturn<K extends keyof PlatformAPI> = Promise<Awaited<MethodReturnType<PlatformAPI, K>>>
-
-// @TODO: replace with responses from IG API
-export type IGThread = Record<string, any>
-export type IGMessage = Record<string, any>
-export type IGParticipant = Record<string, any>
