1ab2a7b74c80f339a50ea5f5fe377cb0a423d9ae
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-07-25 22:27:22 -0700

    update

--- scripts/addToDB.mjs
+++ scripts/addToDB.mjs
@@ -5,29 +5,37 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 // import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 import Database from "better-sqlite3";
 import * as schema from "/Users/rahulvaidun/Texts/platform-instagram/dist/store/schema.js";
+import { sql } from "drizzle-orm";
 
-const getDB = async () => {
-  const sqlite = new Database("/Users/rahulvaidun/Texts/test.db");
-  const db = drizzle(sqlite);
-  // await migrate(db, { migrationsFolder: '/Users/rahulvaidun/Texts/platform-instagram/drizzle' })
-  return db;
-};
-// const rawPayload = fs.readFileSync("scripts/dbtest.json");
-// const rawd = parseRawPayload(rawPayload);
-const db = await getDB();
+const sqlite = new Database("/Users/rahulvaidun/Texts/test122.db");
+const db = drizzle(sqlite);
+// await migrate(db, {
+//   migrationsFolder: "/Users/rahulvaidun/Texts/platform-instagram/drizzle",
+// });
+// return db;
+
+const rawPayload = fs.readFileSync("scripts/dbtest.json");
+const rawd = parseRawPayload(rawPayload);
 
 // if (rawd.deleteThenInsertThread) addThreads(rawd.deleteThenInsertThread);
 const dbRet = await db
   .insert(schema.reactions)
   .values({
     threadKey: "105868004148209",
-    timestampMs: "asdasd",
+    timestampMs: new Date().getTime(),
     messageId: "mid.$cAAA7H5r_VlCPuuV-EGJiaA4MKwFx",
     actorId: "100428318021025",
     reaction: "❤️",
   })
   .returning({ insertedId: schema.reactions.threadKey });
-console.log("inserted");
+
+const result = await db
+  .select({
+    threadKey: schema.reactions.threadKey,
+  })
+  .from(schema.reactions);
+sqlite.close();
+console.log("inserted, ", result[0]);
 // if (rawd.verifyContactRowExists) addUsers(rawd.verifyContactRowExists);
 // if (rawd.addParticipantIdToGroupThread)
 // addParticipants(rawd.addParticipantIdToGroupThread);
