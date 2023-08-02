60b99ddcc84da8525243a690879cf6554a7f0381
batuhan <batuhan@icoz.co>
2023-08-02 23:50:03 +0200

    init

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,19 @@
 import { texts } from '@textshq/platform-sdk'
 
 export const getLogger = (_p?: string) => {
-  const p = `[instagram ${_p ? `(${_p})` : ''}]`
-  const log = (...args: any[]) => {
-    texts.log([p, ...args].map((arg: any) => {
+  const logger = (method: 'log' | 'error', type: 'debug' | 'info' | 'warn' | 'error' | 'fatal') => (...args: (string | object | boolean)[]) =>
+    texts[method]([`[instagram ${_p ? `(${_p})` : ''}]`, type, new Date(), ...args].map(arg => {
       if (typeof arg === 'object') {
         return JSON.stringify(arg, null, 2)
       }
       return arg
     }).join(' '))
-  }
 
   return {
-    debug: log,
-    info: log,
-    warn: log,
-    error: (...args: any[]) => texts.error([p, ...args].join(' ')),
-    fatal: (...args: any[]) => texts.error([p, '[fatal]', ...args].join(' ')),
+    debug: logger('log', 'debug'),
+    info: logger('log', 'info'),
+    warn: logger('log', 'warn'),
+    error: logger('error', 'error'),
+    fatal: logger('error', 'fatal'),
   }
 }
