3263f7e9ef1e24e83ae3a39a31763b44c8f1bc14
batuhan <batuhan@icoz.co>
2023-08-30 02:17:37 +0200

    comment out sec-* header

--- src/constants.ts
+++ src/constants.ts
@@ -9,14 +9,14 @@ export const META_MESSENGER_ENV: EnvironmentKey = 'IG'
 // @TODO: needs to be updated
 export const SHARED_HEADERS = {
   'accept-language': 'en-US,en;q=0.9',
-  'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114"',
-  'sec-ch-ua-full-version-list': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"macOS"',
-  'sec-ch-ua-platform-version': '"13.5.0"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-origin',
+  // 'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114"',
+  // 'sec-ch-ua-full-version-list': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198"',
+  // 'sec-ch-ua-mobile': '?0',
+  // 'sec-ch-ua-platform': '"macOS"',
+  // 'sec-ch-ua-platform-version': '"13.5.0"',
+  // 'sec-fetch-dest': 'empty',
+  // 'sec-fetch-mode': 'cors',
+  // 'sec-fetch-site': 'same-origin',
   'viewport-width': '1280',
 } as const
 
