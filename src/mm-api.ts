5209163b7a63bfff0dca9e9a10380bbca79f18f5
batuhan içöz <batuhan@icoz.co>
2023-11-23 23:29:20 +0100

    fix initial page req headers
    Co-Authored-By: zero <108243503+0xzer@users.noreply.github.com>

--- src/mm-api.ts
+++ src/mm-api.ts
@@ -159,13 +159,12 @@ export default class MetaMessengerAPI {
       const { body } = await this.httpRequest(this.papi.envOpts.initialURL, {
         // todo: refactor headers
         headers: {
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-          'cache-control': 'max-age=0',
-          'sec-ch-ua':
-            '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          // 'cache-control': 'max-age=0',
+          'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
           'sec-fetch-dest': 'document',
           'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
           'sec-fetch-user': '?1',
           'upgrade-insecure-requests': '1',
           'viewport-width': '1280',
