ca4a19aa28c2d2dfa12ddfe1588442911d9338c7
batuhan <batuhan@icoz.co>
2024-01-27 03:07:47 +0100

    `sec-ch-ua` is already handled, set `cache-control` same as browser

--- src/mm-api.ts
+++ src/mm-api.ts
@@ -145,8 +145,7 @@ export default class MetaMessengerAPI {
         // todo: refactor headers
         headers: {
           accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-          // 'cache-control': 'max-age=0',
-          'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="118", "Google Chrome";v="118"',
+          'cache-control': 'max-age=0',
           'sec-fetch-dest': 'document',
           'sec-fetch-mode': 'navigate',
           'sec-fetch-site': 'none',
